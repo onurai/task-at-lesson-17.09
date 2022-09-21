e23864fa413b8804b6ba20a502362325adbb49af
onurai <onurai@code.edu.az>
2022-09-21 17:50:07 +0400

    first attempt

---
+++ task at lesson 17.09/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task_at_lesson_17._09
+{
+    class Program
+    {
+        private static int amount;
+
+
+
+        public static void Main(string[] args)
+        {
+            #region Task 1
+            List<Student> Students = new List<Student>();
+
+            Students.Add(new Student("onur", "ibadov", "turizm", 23));
+            Students.Add(new Student("perviz", "pervizov", "iqtisadci", 25));
+            Students.Add(new Student("turxay", "memmedov", "IT", 26));
+            Students.Add(new Student("valeh", "aliyev", "muhasibat", 34));
+            Students.Add(new Student("ali", "veliyev", "idman", 27));
+
+
+
+            foreach (Student items in Students)
+            {
+                Console.WriteLine(items);
+                //Console.WriteLine(items.ToString());
+            }
+            #endregion
+
+
+            #region Task 2 - INTEREST RATE + EXCEPTION HANDLING
+
+            //decimal money;
+
+            //try
+            //{
+            //    Console.WriteLine("Meblegi daxil edin");
+            //    money = decimal.Parse(Console.ReadLine());
+            //}
+
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("QAQA REQEM DAXIL ELE");
+            //    return;
+            //}
+
+            //Console.WriteLine("Ili daxil edin-");
+            //int year = int.Parse(Console.ReadLine());
+            //int percent = 6;
+
+            //var last_amount = money + (year * ((money * percent) / 100));
+            //Console.WriteLine(last_amount);
+
+            //FINALLY - Exception cixsa da, cixmasa da yene davaMJJS edir.
+            #endregion
+
+
+            #region COMPOUND INTEREST RATE
+
+            //double Total = 0, interestRate, years, annualCompound, Amount;
+
+            //Console.Write("Enter the Initial Amount : ");
+            //Amount = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter the Rate of Interest : ");
+            //interestRate = Convert.ToDouble(Console.ReadLine()) / 100;
+
+            //Console.Write("Enter the Number of Years : ");
+            //years = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Number of Times the Interest will be Compounded : ");
+            //annualCompound = Convert.ToDouble(Console.ReadLine());
+
+            //for (int t = 1; t < years + 1; t++)
+            //{
+            //    Total = Amount * Math.Pow((1 + interestRate / annualCompound),
+            //                             (annualCompound * t));
+
+            //    Console.Write("Your Total for Year {0} " + "is {1:F0}. \n", t, Total);
+
+            //}
+
+            //Console.ReadLine();
+            #endregion
+
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ task at lesson 17.09/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_at_lesson_17._09
+{
+    public class Student
+    {
+        #region task 1
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Faculty { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, string surname, string faculty, int age)
+        {
+            num++;
+            this.Id = num;
+            this.Name = name;
+            this.Name = surname;
+            this.Faculty = faculty;
+            this.Age = age;
+        }
+        public override string ToString()
+        {
+            return Id + " " + Name + " " + Surname + " " + Faculty + " " + Age;
+        }
+
+        public static int num = 100;
+        #endregion
+
+
+
+
+
+
+
+    }
+}
